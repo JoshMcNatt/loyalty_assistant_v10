a253bdb237f2a9ec987d58d833199426c1dd4d7b
JoshMcNatt <103068918+JoshMcNatt@users.noreply.github.com>
2025-02-12 15:02:05 -0500

    rolling back

--- forester_app/forrester_app.py
+++ forester_app/forrester_app.py
@@ -10,27 +10,29 @@ import re
 import streamlit as st
 from forester_prompt_v09 import get_system_prompt
 
-
 st.sidebar.image('forester_app/images/Kobie_Alchemy_Loyalty_Cloud.png', use_column_width=True)
 #st.sidebar.image('Kobie_Alchemy_Loyalty_Cloud.png', use_container_width=True)
 
 st.sidebar.header("Specifications")
 st.sidebar.markdown(
+##    "This application uses OpenAI's GPT-4 to generate marketing campaign ideas and insights based on client-specific customer segmentations, ML-driven recommendations, and other attributes."
 """
 Model: OpenAI's GPT-4   
-Vertical: Travel & Hospitality  
+Vertical: Retail  
 """)
 
 # Initialize the chat messages history
-openai.api_key = st.secrets["openai"]["api_key"]
+openai.api_key = st.secrets.OPENAI_API_KEY
 if "messages" not in st.session_state:
+    # system prompt includes table information, rules, and prompts the LLM to produce
+    # a welcome message to the user.
     st.session_state.messages = [{"role": "system", "content": get_system_prompt()}]
 
 # Prompt for user input and save
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# Display the existing chat messages
+# display the existing chat messages
 for message in st.session_state.messages:
     if message["role"] == "system":
         continue
@@ -45,6 +47,10 @@ if st.session_state.messages[-1]["role"] != "assistant":
         response = ""
         resp_container = st.empty()
         for delta in openai.ChatCompletion.create(
+
+            #model=st.secrets.MODEL_VERSION,
+            #model="gpt-4",
+            #change model type from original to 3.5
             model="gpt-4-1106-preview",
             messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
             stream=True,
@@ -58,16 +64,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
         sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
         if sql_match:
             sql = sql_match.group(1)
-            conn = st.experimental_connection(
-                "snowpark",
-                account=st.secrets["connections"]["snowpark"]["account"],
-                user=st.secrets["connections"]["snowpark"]["user"],
-                role=st.secrets["connections"]["snowpark"]["role"],
-                password=st.secrets["connections"]["snowpark"]["password"],
-                warehouse=st.secrets["connections"]["snowpark"]["warehouse"],
-                database=st.secrets["connections"]["snowpark"]["database"],
-                schema=st.secrets["connections"]["snowpark"]["schema"]
-            )
+            conn = st.experimental_connection("snowpark")
             message["results"] = conn.query(sql)
             st.dataframe(message["results"])
         st.session_state.messages.append(message)
