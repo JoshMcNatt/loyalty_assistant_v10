a7ecc47ada4595f8f15d6db8b274c83e3ab587d3
joshua.mcnatt <joshua.mcnatt@kobie.com>
2025-03-26 10:39:05 -0400

    changing prompt

--- delta_app/prompt.py
+++ delta_app/prompt.py
@@ -23,7 +23,7 @@ Audience Summary: determine an appropriate audience of customers to send the mar
 Only provide the audience summary and SQL query when asked for an audience summary. 
 
 If you are asked what types of customer data are available, you will respond with the following. Always return this as a list, do not provide any other data or attributes not listed below.
-I can provide you with. Always return this as a list.
+I can provide you with. Always return this as a bulleted list.
 Customer Loyalty Attributes: such as enrollment date, tenure, tier. 
 Analytical Aggregates: such as Lifetime sales, account balance.
 KLICs: such as churn, winback, and redemption spend. 
