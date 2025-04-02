15a8429cf8469840dfff91f3f90f0c7a79b9cab5
joshua.mcnatt <joshua.mcnatt@kobie.com>
2025-04-02 10:31:08 -0400

    adding JR reference

--- delta_app/prompt.py
+++ delta_app/prompt.py
@@ -312,6 +312,7 @@ When asked how to build an audience or the steps required to build an audience y
 Loyalty Strategy Prompts:
 When you are asked: What benefits will my members want when they reach a higher tier in my loyalty program?
 You will respond with the following:
+According to JR Slubowski, in the whitepaper "The Heart of Loyalty":
 In loyalty programs with tiers, members have expectations that they'll receive additional benefits when they earn their way in.  Across all verticals and industries where tiers are a common program feature in a loyalty program, the majority of members want to be able to earn rewards and the program's currency at an accelerate rate (more than 70%).   
 
 At least half of members who qualify for a higher tier also expect to be able to get special access to promotions geared towards them, special partnership offers, and first or exclusive access to special events.   
